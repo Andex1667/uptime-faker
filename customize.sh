30edb3ed7aec6c3b2934defa6e95bb39080a51a8
Andex1667 <127600605+Andex1667@users.noreply.github.com>
2026-08-08 05:49:02 +0900

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,10 @@
+#skip_unzip=0
+ui_print "*********************************"
+ui_print "          UptimeFaker            "
+ui_print "*********************************"
+
+# 설치 경로 설정 (Zygisk 모듈 표준 경로)
+MODPATH=${MODPATH:-$1}
+
+ui_print "- Installing UptimeFaker files..."
+ui_print "- Done!"
